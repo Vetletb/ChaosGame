5c53b93e355f3f095169d84003bacea88a7240f9
Vetle <vetletb@stud.ntnu.no>
2024-02-23 14:59:56 +0100

    redid project setup

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module edu.ntnu.idatt2003.chaosgame {
-  requires javafx.controls;
-  requires javafx.fxml;
-
-
-  opens edu.ntnu.idatt2003.chaosgame to javafx.fxml;
-  exports edu.ntnu.idatt2003.chaosgame;
-}
