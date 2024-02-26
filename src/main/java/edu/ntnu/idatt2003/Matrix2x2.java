9da9f4ef49dea83e6352fca18aa587d65ad96cc8
Vetle <vetletb@stud.ntnu.no>
2024-02-26 14:12:06 +0100

    Created Matrix2x2 class and test

---
+++ src/main/java/edu/ntnu/idatt2003/Matrix2x2.java
@@ -0,0 +1,21 @@
+package edu.ntnu.idatt2003;
+
+/**
+ * A class representing a 2x2 matrix.
+ */
+public class Matrix2x2 {
+  private double a00;
+  private double a01;
+  private double a10;
+  private double a11;
+
+  /**
+   * Constructor for the Matrix2x2 class.
+   */
+  public Matrix2x2(double a00, double a01, double a10, double a11) {
+    this.a00 = a00;
+    this.a01 = a01;
+    this.a10 = a10;
+    this.a11 = a11;
+  }
+}
---
+++ src/test/java/edu/ntnu/idatt2003/Matrix2x2Test.java
@@ -0,0 +1,7 @@
+package edu.ntnu.idatt2003;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Matrix2x2Test {
+
+}
