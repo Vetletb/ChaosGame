5cf06b8835620b908fe3c5547d4385e87fc2d958
Vetle <vetletb@stud.ntnu.no>
2024-02-26 14:12:44 +0100

    Created multiply method for Matrix2x2

--- src/main/java/edu/ntnu/idatt2003/Matrix2x2.java
+++ src/main/java/edu/ntnu/idatt2003/Matrix2x2.java
@@ -18,4 +18,13 @@ public class Matrix2x2 {
     this.a10 = a10;
     this.a11 = a11;
   }
+
+  /**
+   * Multiplies this matrix with the given vector.
+   */
+  public Vector2D multiply(Vector2D vector) {
+    double x0 = a00 * vector.getX0() + a01 * vector.getX1();
+    double x1 = a10 * vector.getX0() + a11 * vector.getX1();
+    return new Vector2D(x0, x1);
+  }
 }
