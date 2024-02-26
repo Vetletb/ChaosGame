b02aa827351c122a759159af630c2b85530e9886
Vetle <vetletb@stud.ntnu.no>
2024-02-26 14:11:07 +0100

    created methods for Vector2D

--- src/main/java/edu/ntnu/idatt2003/Vector2D.java
+++ src/main/java/edu/ntnu/idatt2003/Vector2D.java
@@ -14,4 +14,42 @@ public class Vector2D {
     this.x0 = x0;
     this.x1 = x1;
   }
+
+  /**
+   * Returns the x0 value of the vector.
+   *
+   * @return the x0 value of the vector.
+   */
+  public double getX0() {
+    return x0;
+  }
+
+  /**
+   * Returns the x1 value of the vector.
+   *
+   * @return the x1 value of the vector.
+   */
+  public double getX1() {
+    return x1;
+  }
+
+  /**
+   * Adds the given vector to this vector.
+   *
+   * @param vector2 the vector to add to this vector.
+   */
+  public void add(Vector2D vector2) {
+    x0 += vector2.getX0();
+    x1 += vector2.getX1();
+  }
+
+  /**
+   * Subtracts the given vector from this vector.
+   *
+   * @param vector2 the vector to subtract from this vector.
+   */
+  public void subtract(Vector2D vector2) {
+    x0 -= vector2.getX0();
+    x1 -= vector2.getX1();
+  }
 }
