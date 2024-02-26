bc616e061d64e59a343935263366239c753c2510
Vetle <vetletb@stud.ntnu.no>
2024-02-26 14:09:53 +0100

    Created Vector2D class and test

---
+++ src/main/java/edu/ntnu/idatt2003/Vector2D.java
@@ -0,0 +1,17 @@
+package edu.ntnu.idatt2003;
+
+/**
+ * A class representing a 2D vector.
+ */
+public class Vector2D {
+  private double x0;
+  private double x1;
+
+  /**
+   * Constructor for the Vector2D class.
+   */
+  public Vector2D(double x0, double x1) {
+    this.x0 = x0;
+    this.x1 = x1;
+  }
+}
