f1b49397cfb218ca619dd51ff589d180c04e840c
florianleucht <fsleucht@stud.ntnu.no>
2024-03-23 13:23:10 +0100

    Created ChaosGameDescription

---
+++ src/main/java/edu/ntnu/idatt2003/ChaosGameDescription.java
@@ -0,0 +1,55 @@
+package edu.ntnu.idatt2003;
+
+import java.util.List;
+
+/**
+ * A class representing a chaos game description.
+ */
+public class ChaosGameDescription {
+  private final List<Transform2D> transforms;
+  private final Vector2D minCoords;
+  private final Vector2D maxCoords;
+
+  /**
+   * Constructor for the ChaosGameDescription class.
+   */
+  public ChaosGameDescription(List<Transform2D> transforms, Vector2D minCoords, Vector2D maxCoords) {
+    validateCoordinates(minCoords, maxCoords);
+    this.transforms = transforms;
+    this.minCoords = minCoords;
+    this.maxCoords = maxCoords;
+  }
+
+  /**
+   * Returns the list of transforms.
+   *
+   * @return the list of transforms.
+   */
+  public List<Transform2D> getTransforms() {
+      return transforms;
+    }
+
+  /**
+   * Returns the minimum coordinates.
+   *
+   * @return the minimum coordinates.
+   */
+  public Vector2D getMinCoords() {
+      return minCoords;
+    }
+
+  /**
+   * Returns the maximum coordinates.
+   *
+   * @return the maximum coordinates.
+   */
+  public Vector2D getMaxCoords() {
+      return maxCoords;
+    }
+
+  private void validateCoordinates(Vector2D minCoords, Vector2D maxCoords) {
+    if (minCoords.getX0() >= maxCoords.getX0() || minCoords.getX1() >= maxCoords.getX1()) {
+      throw new IllegalArgumentException("minCoords must be less than maxCoords");
+    }
+  }
+}
