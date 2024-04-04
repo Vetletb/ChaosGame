7d4a30ee1d31ef023d368c8e2f08e78b90731c01
Vetle <vetletb@stud.ntnu.no>
2024-04-04 13:12:20 +0200

    added observer handling for ChaosGame

--- src/main/java/edu/ntnu/idatt2003/model/game/ChaosGame.java
+++ src/main/java/edu/ntnu/idatt2003/model/game/ChaosGame.java
@@ -1,14 +1,16 @@
-package edu.ntnu.idatt2003;
+package edu.ntnu.idatt2003.model.game;
 
+import edu.ntnu.idatt2003.model.math.mathModel.Vector2D;
+import edu.ntnu.idatt2003.model.math.transformation.Transform2D;
 import java.util.List;
 import java.util.Random;
 
 /**
  * A class representing a chaos game.
  */
-public class ChaosGame {
+public class ChaosGame extends Subject {
   private final ChaosCanvas canvas;
-  private final ChaosGameDescription description;
+  private ChaosGameDescription description;
   private Vector2D currentPoint;
   private final Random random;
 
@@ -36,6 +38,31 @@ public class ChaosGame {
   }
 
   /**
+   * Sets the description of the chaos game.
+   */
+  private void setDescription(ChaosGameDescription description) {
+    this.description = description;
+  }
+
+  /**
+   * Resets the canvas in the chaos game.
+   */
+  public void resetGame() {
+    canvas.clear();
+    currentPoint = new Vector2D(0, 0);
+  }
+
+  /**
+   * Resets the canvas and sets new description for the chaos game.
+   *
+   * @param description the description to reset the chaos game with.
+   */
+  public void resetGameWithDescription(ChaosGameDescription description) {
+    setDescription(description);
+    resetGame();
+  }
+
+  /**
    * Runs the chaos game for the given number of steps.
    *
    * @param steps the number of steps to run the chaos game for.
@@ -46,6 +73,7 @@ public class ChaosGame {
       Transform2D currentTransformation = transforms.get(random.nextInt(transforms.size()));
       currentPoint = currentTransformation.transform(currentPoint);
       canvas.putPixel(currentPoint);
+      super.notifyObservers();
     }
   }
 }
