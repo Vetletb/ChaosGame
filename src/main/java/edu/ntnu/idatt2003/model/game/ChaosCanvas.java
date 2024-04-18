288ffa3756ec570f9755933bd58b336d88dfcb7f
florianleucht <fsleucht@stud.ntnu.no>
2024-04-18 11:41:38 +0200

    added newPixel

--- src/main/java/edu/ntnu/idatt2003/model/game/ChaosCanvas.java
+++ src/main/java/edu/ntnu/idatt2003/model/game/ChaosCanvas.java
@@ -8,13 +8,13 @@ import edu.ntnu.idatt2003.model.math.transformation.AffineTransform2D;
  * A class representing a canvas for drawing chaos games.
  */
 public class ChaosCanvas {
-
   private int[][] canvas;
   private final int width;
   private final int height;
   private final Vector2D maxCoords;
   private final Vector2D minCoords;
   private AffineTransform2D transformCoordsToIndices;
+  private int[] newPixel;
 
   /**
    * Constructor for the ChaosCanvas class.
@@ -90,7 +90,9 @@ public class ChaosCanvas {
     Vector2D pixel = transformCoordsToIndices.transform(point);
     int x = (int) pixel.getX0();
     int y = (int) pixel.getX1();
-    canvas[x][y] = 1;
+    canvas[x][y] += 1;
+    int value = canvas[x][y];
+    newPixel = new int[]{x, y, value};
   }
 
   /**
@@ -107,5 +109,14 @@ public class ChaosCanvas {
    */
   public void clear() {
     canvas = new int[width][height];
+  }
+
+  /**
+   * Returns the new pixel.
+   *
+   * @return the new pixel.
+   */
+  public int[] getNewPixel() {
+    return newPixel;
   }
 }
