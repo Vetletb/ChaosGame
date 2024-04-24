509a2b2998fc286503d75ba86b2bd5df0fdc8592
Vetle <vetletb@stud.ntnu.no>
2024-04-24 12:18:24 +0200

    updated ChaosGameApp for topBar

--- src/main/java/edu/ntnu/idatt2003/view/ChaosGameApp.java
+++ src/main/java/edu/ntnu/idatt2003/view/ChaosGameApp.java
@@ -1,7 +1,9 @@
 package edu.ntnu.idatt2003.view;
 
 import edu.ntnu.idatt2003.controller.ChaosGameController;
+import edu.ntnu.idatt2003.view.components.TopBar;
 import edu.ntnu.idatt2003.view.components.ViewCanvas;
+import java.io.File;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.concurrent.Service;
@@ -11,6 +13,8 @@ import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
@@ -24,11 +28,6 @@ public class ChaosGameApp extends Application {
 
   @Override
   public void start(Stage stage) throws Exception {
-    Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
-    HBox root = new HBox();
-    viewCanvas = new ViewCanvas(800, 800);
-    controller = new ChaosGameController(viewCanvas, 800, 800);
-    root.getChildren().add(viewCanvas.getCanvas());
 
     service = new Service<Void>() {
       @Override
@@ -38,7 +37,7 @@ public class ChaosGameApp extends Application {
           protected Void call() throws Exception {
             for (int i = 0; i < 100000; i++) {
               Platform.runLater(() -> controller.runSteps(1));
-              Thread.sleep(0, 1);
+              Thread.sleep(0,1);
             }
             return null;
           }
@@ -46,15 +45,32 @@ public class ChaosGameApp extends Application {
       }
     };
 
-    Button button = new Button("Run 100000 steps");
-    button.setOnAction(e -> {
-      if (service.getState() == Worker.State.SUCCEEDED) {
-        service.restart();
-      } else {
-        service.start();
-      }
-    });
-    root.getChildren().add(button);
+    Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
+    VBox root = new VBox();
+    viewCanvas = new ViewCanvas(800, 800);
+    controller = new ChaosGameController(viewCanvas, 800, 800);
+    HBox topBar = new TopBar(controller, service);
+    root.getChildren().addAll(
+        topBar,
+        viewCanvas.getCanvas());
+
+
+//    Button button = new Button("Run 100000 steps");
+//    button.setOnAction(e -> {
+//      if (service.getState() == Worker.State.SUCCEEDED) {
+//        service.restart();
+//      } else {
+//        service.start();
+//      }
+//    });
+//    root.getChildren().add(button);
+
+//    Button readFileButton = new Button("Read file");
+//    readFileButton.setOnAction(e -> {
+//      FileChooser fileChooser = new FileChooser();
+//      fileChooser.setTitle("Open File");
+//      File file = fileChooser.showOpenDialog(new Stage());
+//    });
 
     Scene scene = new Scene(root, screenBounds.getWidth(), screenBounds.getHeight());
 
