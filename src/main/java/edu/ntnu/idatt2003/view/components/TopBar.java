32e9d8fccbf517ad94bc18abaf956a07956669c3
Vetle <vetletb@stud.ntnu.no>
2024-04-24 12:17:36 +0200

    Created topBar class

---
+++ src/main/java/edu/ntnu/idatt2003/view/components/TopBar.java
@@ -0,0 +1,74 @@
+package edu.ntnu.idatt2003.view.components;
+
+import edu.ntnu.idatt2003.controller.ChaosGameController;
+import javafx.concurrent.Service;
+import javafx.concurrent.Worker;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+
+public class TopBar extends HBox {
+  private ChaosGameController controller;
+  private Service<Void> service;
+
+  public TopBar(ChaosGameController controller, Service<Void> service) {
+    super();
+    this.controller = controller;
+    this.service = service;
+
+    Button juliaButton = new Button("Julia Set");
+    juliaButton.setOnAction(e -> {
+      this.controller.resetChaosGame("Julia Set");
+    });
+
+    Button sierpinskiButton = new Button("Sierpinski");
+    sierpinskiButton.setOnAction(e -> {
+      this.controller.resetChaosGame("Sierpinski");
+    });
+
+    Button barnsleyButton = new Button("Barnsley");
+    barnsleyButton.setOnAction(e -> {
+      this.controller.resetChaosGame("Barnsley");
+    });
+
+    Button readFileButton = new Button("Read File");
+    readFileButton.setOnAction(e -> {
+      System.out.println("Read File");
+    });
+
+    Button writeFileButton = new Button("Write File");
+    writeFileButton.setOnAction(e -> {
+      System.out.println("Write File");
+    });
+
+    Button editButton = new Button("Edit");
+    editButton.setOnAction(e -> {
+      System.out.println("Edit");
+    });
+
+    TextField iterationsField = new TextField();
+    iterationsField.setPromptText("Iterations");
+
+    Button runButton = new Button("Run");
+    runButton.setOnAction(e -> {
+      int iterations = Integer.parseInt(iterationsField.getText());
+      if (service.getState() == Worker.State.SUCCEEDED) {
+        service.restart();
+      } else {
+        service.start();
+      }
+    });
+
+    this.setSpacing(10);
+
+    this.getChildren().addAll(
+        juliaButton,
+        sierpinskiButton,
+        barnsleyButton,
+        readFileButton,
+        writeFileButton,
+        editButton,
+        iterationsField,
+        runButton);
+  }
+}
