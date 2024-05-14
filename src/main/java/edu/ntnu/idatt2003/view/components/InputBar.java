0a2f11295958eb259c4c6005fceea24cd83ec172
florianleucht <fsleucht@stud.ntnu.no>
2024-05-14 13:27:41 +0200

    Added max characters to inputbar

--- src/main/java/edu/ntnu/idatt2003/view/components/InputBar.java
+++ src/main/java/edu/ntnu/idatt2003/view/components/InputBar.java
@@ -3,10 +3,18 @@ package edu.ntnu.idatt2003.view.components;
 import javafx.scene.control.TextField;
 
 public class InputBar extends TextField {
-    public InputBar() {
-        super();
-        this.setPrefWidth(150);
-        this.setMaxHeight(18);
-        this.getStyleClass().add("input-bar");
+  private static final int MAX_CHARS = 10;
+
+  public InputBar() {
+    super();
+    this.setPrefWidth(150);
+    this.setMaxHeight(18);
+    this.getStyleClass().add("input-bar");
+
+    this.textProperty().addListener((observable, oldValue, newValue) -> {
+      if (newValue.length() > MAX_CHARS) {
+        this.setText(oldValue);
+      }
+    });
     }
 }
