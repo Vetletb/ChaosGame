ef9a7e8c8d7a7ddf1448d415c27e116c72ac9bbe
florianleucht <fsleucht@stud.ntnu.no>
2024-05-15 16:23:09 +0200

    refactored package structure

---
+++ src/main/java/edu/ntnu/idatt2003/view/components/popups/SuccessPopup.java
@@ -0,0 +1,19 @@
+package edu.ntnu.idatt2003.view.components.popups;
+
+/**
+ * A popup that displays a success message.
+ */
+public class SuccessPopup extends MessagePopup {
+  public SuccessPopup() {
+    super();
+    setWrapperStyle();
+  }
+
+  /**
+   * Sets the style of the wrapper.
+   */
+  @Override
+  void setWrapperStyle() {
+    messageWrapper.getStyleClass().add("success-popup");
+  }
+}
